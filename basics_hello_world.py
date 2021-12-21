25b192de77d4f67f4c84d79d9ca220a34f8dc1b4
munnishaik2064 <munnishaik2064@gmail.com>
2021-12-21 16:42:13 +0530

    Hello_world file

---
+++ basics_hello_world.py
@@ -0,0 +1,11 @@
+#print("hello world")
+#import keyword
+#print(keyword.kwlist) 
+def m1():
+    a=10
+    print(10)
+    return "hai"
+print(m1())
+print(''' hello "me" ''')
+print("hai is \\ ")
+print("hai \v helloooooo")
